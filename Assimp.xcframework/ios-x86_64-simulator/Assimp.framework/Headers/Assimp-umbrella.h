a658b6359aed8ab8670d20468140d73b284fbd3a
Adrian Andreca <adrian.andreca@gmail.com>
2020-08-27 16:50:53 +0400

    assimp simulator support

---
+++ Assimp.xcframework/ios-x86_64-simulator/Assimp.framework/Headers/Assimp-umbrella.h
@@ -0,0 +1,51 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "pushpack1.h"
+#import "poppack1.h"
+#import "pstdint.h"
+#import "cimport.h"
+#import "cexport.h"
+#import "aabb.h"
+#import "ai_assert.h"
+#import "anim.h"
+#import "camera.h"
+#import "cfileio.h"
+#import "color4.h"
+#import "config.h"
+#import "defs.h"
+#import "importerdesc.h"
+#import "light.h"
+#import "material.h"
+#import "matrix3x3.h"
+#import "matrix4x4.h"
+#import "mesh.h"
+#import "metadata.h"
+#import "pbrmaterial.h"
+#import "postprocess.h"
+#import "quaternion.h"
+#import "scene.h"
+#import "texture.h"
+#import "types.h"
+#import "vector2.h"
+#import "vector3.h"
+#import "version.h"
+#import "vector3.inl"
+#import "vector2.inl"
+#import "color4.inl"
+#import "matrix3x3.inl"
+#import "matrix4x4.inl"
+#import "quaternion.inl"
+
+FOUNDATION_EXPORT double AssimpVersionNumber;
+FOUNDATION_EXPORT const unsigned char AssimpVersionString[];
+
